41eebf4cd89eb3451e249a20ef2d18a4088521ad
Governikus <ausweisapp2@governikus.de>
2023-03-09 17:48:10 +0100

    Add revision: v1.26.3

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+	name: "AusweisApp2SDKWrapper",
+	products: [
+		.library(
+			name: "AusweisApp2SDKWrapper",
+			targets: ["AusweisApp2Dependency", "AusweisApp2SDKWrapper"]
+		)
+	],
+	dependencies: [
+		.package(
+			name: "AusweisApp2",
+			url: "https://github.com/Governikus/AusweisApp2-SDK-iOS",
+			from: "1.26.3"
+		)
+	],
+	targets: [
+		.target(
+			name: "AusweisApp2Dependency", // Dummy target, so we can depend on the AusweisApp2, as binaryTarget can't.
+			dependencies: ["AusweisApp2"]
+		),
+		.binaryTarget(
+			name: "AusweisApp2SDKWrapper",
+			path: "AusweisApp2SDKWrapper.xcframework"
+		)
+	]
+)
