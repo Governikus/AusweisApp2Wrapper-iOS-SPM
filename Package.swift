7fed0ff7e14f950bf68b0da16872dec97e1b5a1c
Governikus <ausweisapp2@governikus.de>
2026-01-30 11:36:45 +0000

    Add revision: v2.4.1

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
 		.package(
 			name: "AusweisApp2",
 			url: "https://github.com/Governikus/AusweisApp2-SDK-iOS",
-			.exact("2.4.0")
+			.exact("2.4.1")
 		)
 	],
 	targets: [
