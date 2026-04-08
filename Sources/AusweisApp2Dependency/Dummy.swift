1bda86ee24585f545b0bc33e45d62a21c2476d84
Governikus <ausweisapp2@governikus.de>
2026-04-08 11:29:37 +0000

    Add revision: v2.5.0

--- Sources/AusweisApp2Dependency/Dummy.swift
+++ Sources/AusweisApp2Dependency/Dummy.swift
@@ -1,8 +1,12 @@
-/**
+/*
  * Copyright (c) 2021-2026 Governikus GmbH & Co. KG, Germany
  */
 
-/**
+/*
+ * Copyright (c) 2021-2026 Governikus GmbH & Co. KG, Germany
+ */
+
+/*
  * This is a simple dummy, so we can have a target that depends on the AusweisApp2.
  * This is, because binaryTargets like the SDKWrapper currently cannot have dependencies:
  * https://bugs.swift.org/browse/SR-14245
